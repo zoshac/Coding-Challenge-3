3e791dbdbdc7de8bfcf4906a33983a1f3235360b
Zosha Chung <czosha@gmail.com>
2025-02-02 16:18:40 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -12,3 +12,21 @@ price.shift();
 console.log("New Price List", price);
 
 //Updated Price list 
+
+
+// Task 2- Order List 
+
+// Quantinty of Orders 
+let orders = [134, 567, 800, 491, 132];
+
+// Increase order quantity three
+orders[800] += 5; 
+
+console.log("Updated orders", orders);
+
+// Total number of orders 
+let sum = 0; 
+for (let i = 0; i < addEventListener.length; i++) {
+    sum+= orders[i];
+}
+console.log("Sum of Orders:", sum);
