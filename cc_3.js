77b0903f0d92d664e4c5e36741278525ec16eb83
Zosha Chung <czosha@gmail.com>
2025-02-02 15:25:53 -0500

    Task 1- Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,14 @@
+// Task 1- Product Pricing System
+
+// Declare Price 
+let price = [10, 20, 30, 40, 50];
+
+// New Price added
+price.push(60);
+
+//Price Removed
+price.shift();
+
+console.log("New Price List", price);
+
+//Updated Price list 
